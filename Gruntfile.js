e281d5e83ed17c08e13b65200cd2536c03bd2a6a
wangluyu1998 <2602649277@qq.com>
2020-03-17 21:34:19 +0800

    html

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    htmlmin:{
+      options:{
+        removeComments:true,
+        collapseWhitespace:true
+      },
+      files:{
+        src:'./index.html',
+        dest:'dist/index.html'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('default',['htmlmin']);
+};
---
+++ rectangle.js
@@ -0,0 +1,19 @@
+$(function(){
+  //get dom
+  var $width  = $('#width'),
+      $height = $('#height');
+  var $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  /*calc button click event*/
+$btnCal.click(function(){
+  var w = Number($width.val()),
+      h = Number($height.val());
+  
+  var p = 2*(w+h),
+      a = w*h;
+  
+  $perimeter.val(p);
+  $area.val(a);
+})
+});
